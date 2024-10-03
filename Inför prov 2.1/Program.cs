40e2824a59142ab527ccd93b9573c62c84405e07
maol07001 <maol07001@E-PW05CGR4>
2024-10-03 13:53:46 +0200

    Add project files.

---
+++ Inför prov 2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken farorit bok har du?");
+            string favbok = Console.ReadLine( );
+            Console.WriteLine(favbok);
+        }
+    }
+}
